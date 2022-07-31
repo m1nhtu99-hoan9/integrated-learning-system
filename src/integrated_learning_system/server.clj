1612caeac701d90ca6d6d1790a77db3cc5c9c9e3
MinhTu Thomas Hoang <minhtu.hoang19@gmail.com>
2022-08-01 02:59:44 +0700

    🔧 Register new 'mulog' console log publisher only when there are none yet

--- src/integrated_learning_system/server.clj
+++ src/integrated_learning_system/server.clj
@@ -1,12 +1,16 @@
 (ns integrated-learning-system.server
   (:require [clojure.spec.alpha :as s]
             [com.brunobonacci.mulog :as mulog]
-            [integrated-learning-system.services :refer [config->service-map]]))
+            [com.brunobonacci.mulog.core :refer [publishers]]
+            [integrated-learning-system.services :refer [config->service-map]])
+  (:import [com.brunobonacci.mulog.publisher ConsolePublisher]))
+
 
 (s/def ::state #{:started :resumed :stopped :restarted :failed})
 (s/def ::env #{:prod :dev :test})
 
-(mulog/start-publisher! {:type :console})
+(if [not-any? #(instance? ConsolePublisher %) @publishers]
+  (mulog/start-publisher! {:type :console, :pretty? true}))
 
 (defn -main [config-fname]
   (config->service-map config-fname))
